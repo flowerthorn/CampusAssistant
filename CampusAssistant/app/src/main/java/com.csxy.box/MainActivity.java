23f7b74535d26d9a36c31bf540ff29881408c457
lihongxin <>
2018-05-05 14:34:26 +0800

    feature:迁移到 Android Plugin for Gradle 3.0.0

--- CampusAssistant/app/src/main/java/com.csxy.box/MainActivity.java
+++ CampusAssistant/app/src/main/java/com.csxy.box/MainActivity.java
@@ -3,7 +3,9 @@ package com.csxy.box;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.csxy.box.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
